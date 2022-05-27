8349e717398505bf9728bc855c4a5c6d08ee2524
mamarezafrzn <mamarezafrz@gmail.com>
2022-05-27 12:49:51 +0430

    v1.3

---
+++ js/stickyItem.js
@@ -0,0 +1,22 @@
+// document.getElementById("stickyMobile").addEventListener('click',stickyMenu())
+// function stickyMenu(){ 
+//     console.log("clicked");
+//     if(document.getElementById("showHide").style.display=="none"){
+//         document.getElementById("showHide").style.display = "block";
+//     }else  if(document.getElementById("showHide").style.display=="block"){
+//         document.getElementById("showHide").style.display=="none";
+//     }
+// }
+
+
+
+
+// window.addEventListener('resize',resetMenu());
+//     function resetMenu(){
+//         if(window.innerWidth >= 900){
+//             document.getElementById("stickyMobile").style.display=="none";
+//         }else if(window.innerWidth < 900){
+//             document.getElementById("stickyMobile").style.display=="block";
+//         }
+   
+// }
